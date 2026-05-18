e8078f6d59bdd25f515a749fd8d188b5a32db7db
gabriel Mina da Silva <gabrielmina118@gmail.com>
2026-05-18 19:50:46 -0300

    feat: :sparkles: inserindo codigos

---
+++ condicionais/index.py
@@ -0,0 +1,6 @@
+def valida_nota(nota):
+    if nota < 5:
+        print("Nota menor que 5")
+
+
+valida_nota(4)
---
+++ index.py
@@ -0,0 +1,42 @@
+# lista = []
+# lista.append("Salve")
+# print(lista)
+
+# lista = ["Ana", "Jose", "Maria", "Janaylla", "Gabriel"]
+
+materias = ("fisica", "Quimica", "biologia", "Matematica")
+
+# tupla_lower = tuple(nome.lower() for nome in tupla)
+
+# print(tupla_lower)
+
+pessoas = {
+    'nome':"Jose da silva",
+    "idade": 50,
+    "cursos":materias 
+}
+
+# print(type(pessoas))
+# print(pessoas.keys())
+
+# pessoas.pop('nome')
+
+# print(pessoas)
+
+# numeros = {1,2,50,53,235,170}
+# print(type(numeros))
+
+# textos = set("Saalvee")
+# print(numeros)
+
+# print(numeros.union(textos))
+
+# nome, idade = "gabriel" , 28
+
+# print(f"Meu nome é {nome} e a minha idade é {idade}")
+
+# if (10 > 5):
+#     print("Maior")
+# else:
+#     print("Menor")
+
